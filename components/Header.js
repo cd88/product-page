9aa88115332aebd357fb49c93007dd81663d295d
celloworld <camerondonahue@gmail.com>
2018-07-16 12:08:35 -0700

    changed bg image and header text

--- components/Header.js
+++ components/Header.js
@@ -10,9 +10,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Mulch Mate</h1>
+                <p>A reusable garden tool that removes the<br/>frustration of bagging yard waste</p>
             </div>
         </div>
         <nav>
