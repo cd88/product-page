1bed7ab3db072f94fa06567f7a81b35f0a31fb94
celloworld <camerondonahue@gmail.com>
2018-07-30 18:52:31 -0700

    made export (out/), migrated _image.scss

--- components/Main.js
+++ components/Main.js
@@ -6,16 +6,97 @@ import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram'
 import faGithub from '@fortawesome/fontawesome-free-brands/faGithub'
 
 class Main extends React.Component {
+
   render() {
 
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
 
     return (
       <div id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+      <style jsx>{`
+        .image {
+      		border-radius: _size(border-radius);
+      		border: 0;
+      		display: inline-block;
+      		position: relative;
 
+      		&:before {
+      			@include vendor('pointer-events', 'none');
+      			background-image: url('/static/images/overlay.png');
+      			background-color: _palette(bg-overlay);
+      			border-radius: _size(border-radius);
+      			content: '';
+      			display: block;
+      			height: 100%;
+      			left: 0;
+      			opacity: 0.5;
+      			position: absolute;
+      			top: 0;
+      			width: 100%;
+      		}
+
+      		img {
+      			border-radius: _size(border-radius);
+      			display: block;
+      		}
+
+      		&.left,
+      		&.right {
+      			max-width: 40%;
+
+      			img {
+      				width: 100%;
+      			}
+      		}
+
+      		&.left {
+      			float: left;
+      			padding: 0 1.5em 1em 0;
+      			top: 0.25em;
+      		}
+
+      		&.right {
+      			float: right;
+      			padding: 0 0 1em 1.5em;
+      			top: 0.25em;
+      		}
+
+      		&.fit {
+      			display: block;
+      			margin: 0 0 _size(element-margin) 0;
+      			width: 100%;
+
+      			img {
+      				width: 100%;
+      			}
+      		}
+
+      		&.main {
+      			display: block;
+      			margin: (_size(element-margin) * 1.25) 0;
+      			width: 100%;
+
+      			img {
+      				width: 100%;
+      			}
+      		}
+
+      		@include breakpoint(small) {
+      			&.main {
+      				margin: (_size(element-margin) * 1) 0;
+      			}
+      		}
+
+      		@include breakpoint(xsmall) {
+      			&.main {
+      				margin: (_size(element-margin) * 0.75) 0;
+      			}
+      		}
+      	}
+      `}</style>
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Intro</h2>
-          <span className="image main"><img src="/static/images/pic01.jpg" alt="" /></span>
+          <span className="image main"><img src=${process.env.BACKEND_URL} + "/static/images/pic01.jpg" alt="" /></span>
           <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
           {close}
@@ -23,7 +104,7 @@ class Main extends React.Component {
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
-          <span className="image main"><img src="/static/images/pic02.jpg" alt="" /></span>
+          <span className="image main"><img src=${process.env.BACKEND_URL} + "/static/images/pic02.jpg" alt="" /></span>
           <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
           <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
           {close}
@@ -31,7 +112,7 @@ class Main extends React.Component {
 
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">About</h2>
-          <span className="image main"><img src="/static/images/pic03.jpg" alt="" /></span>
+          <span className="image main"><img src=${process.env.BACKEND_URL} + "/static/images/pic03.jpg" alt="" /></span>
           <p>Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices. Aliquam libero et malesuada fames ac ante ipsum primis in faucibus. Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit amet.</p>
           {close}
         </article>
