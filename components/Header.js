61d4507734a933b7f4b33f02d710b6d4d6920aa0
celloworld <camerondonahue@gmail.com>
2018-08-25 14:21:04 -0700

    renamed articles, setting up slides

--- components/Header.js
+++ components/Header.js
@@ -17,8 +17,8 @@ const Header = (props) => (
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('demo')}}>Demo</a></li>
+                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('purchase')}}>Purchase</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
