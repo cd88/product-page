1606d0f7bc55909183682ea2e6769ecfbe2f6d69
celloworld <camerondonahue@gmail.com>
2018-08-25 19:58:04 -0700

    moved demo data, added images
    got through several errors but still implementing react-touch-carousel. I'm attempting to get the 'boring' example working in my project with my data

---
+++ static/demo-slide-data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    title: 'Step 1',
+    text: "Insert the Mulchmate diagonally downward into a Lawn Bag",
+    image: "static/images/mulchmate-demo-slide-1"
+  },
+  {
+    title: 'Step 2',
+    text: "Lift and guide the Mulchmate corners until snug against the bag corners",
+    image: "static/images/mulchmate-demo-slide-2"
+  },
+  {
+    title: 'Step 3',
+    text: "<strong>Ready to use!</strong> The bag will stand &amp; remain open while you fill it",
+    image: "static/images/mulchmate-demo-slide-3"
+  }
+]
+
+export default data
