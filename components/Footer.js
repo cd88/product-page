025821576a25c3c8b84a6b08ff6b6e47b0edbac1
celloworld <camerondonahue@gmail.com>
2018-10-02 11:37:08 -0700

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy;2019 The Mulchmate &mdash; <a href="http://www.paulsonindustries.net/">Paulson Industries, LLC</a><br/>Design by <a href="https://github.com/celloworld">FuturePilots</a></p>
+        <p className="copyright">&copy;2019 The Mulchmate &mdash; <a href="http://www.paulsonindustries.net/">Paulson Industries, LLC</a></p>
     </footer>
 )
 
