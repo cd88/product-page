1dfa01e4385ef18dc39c00c9b505e53bd96bc566
celloworld <camerondonahue@gmail.com>
2018-10-05 13:55:03 -0700

    added stripe token env var and handler.js

--- serverless/handler.js
+++ serverless/handler.js
@@ -10,5 +10,23 @@ module.exports.hello = async (event, context) => {
 
 };
 
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+module.exports.getProductData = (event, context, callback) => {
+  return stripe.products.list({limit: 10})
+    .then((products) => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({
+          data: products.data
+        }),
+      };
+      callback(null, response);
+});
+
+
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
